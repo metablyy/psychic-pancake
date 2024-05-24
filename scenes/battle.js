b3bf411d226f096803d20243bb5f0f9581e482d4
ben <115721234+metablyy@users.noreply.github.com>
2024-05-24 12:29:19 +0000

    attempt 5000 of trying working commits

--- scenes/battle.js
+++ scenes/battle.js
@@ -291,7 +291,9 @@ export function makeBattle(p) {
           this.currentState = states.winnerDeclared;
 
           setTimeout(function(){
-            endCleanup(); // calling it like this may work?
+
+            endCleanup(); 
+            // calling it like this may work?
           }, 5000)
 
           // return;
@@ -308,6 +310,7 @@ export function makeBattle(p) {
           p.image(this.battleBackgroundImage, 0, 0);
           
           setTimeout(function(){
+            
             endCleanup();
           }, 5000)
           
